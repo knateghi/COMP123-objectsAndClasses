39d1cbdc6137e26d124c171fd21cf65dded00c69
knateghi <knateghi@my.centennialcollege.ca>
2019-12-29 13:57:57 -0500

    Added the Person class

--- Person.cs
+++ Person.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name: Kobra Nateghi
+ * Description: this project demonstrates 
+ * the use of objects and classes
+ */
 namespace COMP123_objectsAndClasses
 {
     class Person
