9913b9b5b6eb7027eca90a79bb6c7d2f1b3f6dab
knateghi <knateghi@my.centennialcollege.ca>
2019-12-29 22:18:57 -0500

    Added the methods and additional constructors to the Person class

--- Person.cs
+++ Person.cs
@@ -12,5 +12,68 @@ namespace COMP123_objectsAndClasses
 {
     public class Person
     {
+        //private intance variables
+        private string _name;
+        private int _age;
+        //public properties
+        public string Name { get => this._name;
+            set
+            {
+               // Console.WriteLine("name value set");
+                this._name = value;
+            }
+        }
+        /// <summary>
+        /// this is a public property for our _age field
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// this is the default empty constructor for the Person
+        /// class
+        /// </summary>
+        /// 
+        // constructors===============================================
+        public Person()
+        {
+            this.Name= "Unknown name";
+        }
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public Person(int age)
+        {
+            this.Age = age;
+        }
+        public Person(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+        //===================================================================
+
+        /// <summary>
+        ///this method outputs the _name value with "says hello" 
+        ///to the console
+        ///@method _sayHello
+        ///@return {void}
+        /// </summary>
+        public void SayHello()
+        {
+            Console.WriteLine(this.Name + " says Hello");
+        }
+
+
+        /// <summary>
+        /// this method outputs the name and the age 
+        /// </summary>
+        public void ShowAge()
+        {
+            Console.WriteLine($"{this.Name} is {this.Age} years old");
+        } 
     }
 }
+
+
--- Program.cs
+++ Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace COMP123_objectsAndClasses
 {
+    /// <summary>
+    /// This class is the driver class for our program
+    /// 
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
+            //creating an insance of the Person class
+            Person tanya = new Person();
+            tanya.Name = "Tanya";
+            tanya.Age = 28;
+            tanya.SayHello();
+            tanya.ShowAge();
+            Console.WriteLine();
+
+            Person tom = new Person("Tom");
+            tom.Age = 47;
+            tom.SayHello();
+            tom.ShowAge();
+            Console.WriteLine();
+
+
+            Person mark = new Person(30);
+            mark.Name = "Mark";
+            mark.SayHello();
+            mark.ShowAge();
+            Console.WriteLine();
+
+
+            Person mary = new Person("Mary", 23);
+            mary.SayHello();
+            mary.ShowAge();
         }
     }
+       
 }
